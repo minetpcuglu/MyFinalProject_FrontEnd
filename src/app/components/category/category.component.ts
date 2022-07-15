607e634ed2c9147d1dd468c20ea26cc3435d7263
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-07-15 18:06:57 +0300

    15.07

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,6 +10,7 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categories :Category[]=[];
+  currentCategory:Category;
   constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
@@ -21,7 +22,25 @@ export class CategoryComponent implements OnInit {
     
     })
     
-  }}
+  }
+  setCurrentCategory(category:Category){
+    
+    this.currentCategory=category;
+
+  }
+
+  getCurrentCategory(category:Category){
+    if(category==this.currentCategory){
+return "list-group-item active"
+    }
+    else{
+      return "list-group-item "
+    }
+  }
+
+
+}
+
 
 
 
